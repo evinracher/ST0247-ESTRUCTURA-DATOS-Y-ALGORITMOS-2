68083d29888403c058bb3a8dfe9d201db0e1011e
Daniel Mesa <damesaa@eafit.edu.co>
2018-02-01 11:33:01 -0500

    subiendo Matriz Adyecente

--- talleres/taller01/codigo/Digraph.java
+++ talleres/taller01/codigo/Digraph.java
@@ -15,7 +15,7 @@ public abstract class Digraph {
 	public abstract void addArc(int source, int destination, int weight);
 
 	public abstract ArrayList<Integer> getSuccessors(int vertex);
-
+	
 	public abstract int getWeight(int source, int destination);
 
 	public int size() {
--- talleres/taller01/codigo/DigraphAM.java
+++ talleres/taller01/codigo/DigraphAM.java
@@ -6,25 +6,63 @@ import java.util.ArrayList;
  * @author Mauricio Toro, Mateo Agudelo, <su nombre>
  */
 public class DigraphAM extends Digraph {
-	// complete...
+	private int [][] grafo;
 
 	public DigraphAM(int size) {
 		super(size);
-		// complete...
+		grafo = new int [size][size];
 	}
 
 	public void addArc(int source, int destination, int weight) {
-		// complete...
-		// recuerde: grafo dirigido!
+		grafo[source][destination] = weight;
 	}
 
 	public ArrayList<Integer> getSuccessors(int vertex) {
-		// complete...
-		// recuerde: null si no hay!
+		ArrayList <Integer> successors = new ArrayList<Integer>();
+		//int k = 0;
+		for(int i = 0; i < size; i++)
+		{
+			if(grafo[vertex][i] != 0)
+			{
+				successors.add(i);
+			}
+		}
+		return successors;
 	}
 
 	public int getWeight(int source, int destination) {
-		// complete...
+		return grafo[source][destination];
 	}
+
+	public void imprimir()
+	{
+		for(int i = 0; i < size; i++)
+		{
+			for(int j = 0; j < size; j++)
+			{
+				System.out.print(grafo[i][j]);
+			}
+			System.out.println();
+		}
+
+	}
+
+	public static void main(String[] args)
+	{
+		DigraphAM grafo1 = new DigraphAM(8);
+		grafo1.addArc(5, 11, 1);
+		grafo1.addArc(7, 8, 1);
+		grafo1.addArc(7, 11, 1);
+		grafo1.addArc(3, 8, 1);
+		grafo1.addArc(3, 10, 1);
+		grafo1.addArc(8, 9, 1);
+		grafo1.addArc(11, 2, 1);
+		grafo1.addArc(11, 9, 1);
+		grafo1.addArc(11, 10, 1);
+
+		grafo1.imprimir();
+
+	}
+
 
 }
