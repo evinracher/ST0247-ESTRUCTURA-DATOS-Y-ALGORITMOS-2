6c817f51a9c07d43919833762ffb9cb3ce6e1ef5
eafit-201710093010 <kaparrah@eafit.edu.co>
2018-02-08 17:01:59 -0500

    Pasando punto 2

--- talleres/taller02/codigo/Taller2.java
+++ talleres/taller02/codigo/Taller2.java
@@ -16,6 +16,26 @@ public class Taller2 {
 	return sumaGrupo(start+1,nums,target)||sumaGrupo(start+1,nums, target-nums[start]);
     }
 
+    public static ArrayList<String> permutations(String s) {
+	ArrayList<String> r = new ArrayList<String>();
+	permutations("", s, r);
+	return r;
+    }
+
+    // recomendacion
+    private static void permutations(String pre, String pos, ArrayList<String> list) {
+	if(pos.length() == 0){
+	    System.out.println(pre);
+	    list.add(pre);
+	}else{
+	    for(int i = 0; i < pos.length(); ++i ){
+		permutations(pre+pos.charAt(i), pos.substring(0,i)+pos.substring(i+1), list);
+	    }
+	}
+    }
+
+    /**
+
     /**    public static ArrayList<String> combinations(String s) {
 	// complete...
     }
@@ -56,5 +76,9 @@ public class Taller2 {
     public static int queens(int n) {
 	// complete...
 	}**/
+    public static void main(String[] args){
+	Taller2 proff = new Taller2();
+	proff.permutations("abc");
+    }
 
 }
