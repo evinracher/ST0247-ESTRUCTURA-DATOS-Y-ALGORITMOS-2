290f67b4fe585e04b523fc8448799c72310f6330
Daniel Mesa <damesaa@eafit.edu.co>
2018-02-25 09:22:52 -0500

    cambios y mejoras

--- talleres/taller02/codigo_estudianteCORRECTO/Taller2obsoleto.java
+++ talleres/taller02/codigo_estudianteCORRECTO/Taller2obsoleto.java
@@ -7,7 +7,7 @@
 import java.lang.*;
 import java.util.*;
 import java.util.ArrayList;
-public class Taller2
+public class Taller2obsoleto
 {
     
     private static ArrayList<String> combinationsAux(String base, String s, ArrayList<String> p)
@@ -40,7 +40,7 @@ public class Taller2
     // recomendacion
     private static void permutations(String pre, String pos, ArrayList<String> list) {
 	if(pos.length() == 0){
-	    //System.out.println(pre);
+	    System.out.println(pre);
 	    list.add(pre);
 	}else{
 	    for(int i = 0; i < pos.length(); ++i ){
@@ -49,77 +49,70 @@ public class Taller2
 	}
     }
 
-    public static int reinas(int numero)
+    public static void reinas(int numero)
     {
-	int resultado = 0;
-	String original = "";
 	int [] tablero = new int[numero];
-	for(int  i = 0; i < numero; i++)
-	    {
-		original = original+i;
-	    }
-	ArrayList<String> todos = new ArrayList<String>();
-	todos = permutations(original);
-	
-	for(int j = 0; j < todos.size(); j++)
-	    {
-		String prueba = todos.get(j);
-		for(int i = 0; i < prueba.length(); i++)
-		    {
-			tablero[i] = Integer.parseInt(prueba.charAt(i)+"");
-		    }
-		if(valido(tablero) == true)
-		    {
-			resultado = resultado + 1;
-		    }
-		//System.out.println(resultado);
-	    }
-	return resultado;
+	//ArrayList<Integer> p = new ArrayList<>();
+	int ta = 0;
+	auxiliar(tablero, 0);
 	//System.out.println(ta);
 	//return ta;
     }
-    
 
-    public static ArrayList<int []> auxiliar(int[] tablero, int  posicion, ArrayList<int[]> todos)
+    public static void auxiliar(int[] tablero, int posicion)
     {
 	if(posicion == tablero.length)
 	    {
-		todos.add(tablero);
+		if(valido(tablero))
+		    {
+			imprimir(tablero);
+		    }
 	    }else
 	    {
 		for(int i = 0; i < tablero.length; i++)
 		    {
 			tablero[posicion] = i;
-			auxiliar(tablero, posicion+1, todos);
+			auxiliar(tablero, posicion+1);
 		    }
 	    }
-	return todos;
     }
 
-    
-    public static void imprimir(int[] arreglo)
+    public static void imprimir(int[] tablero)
     {
-	for(int i = 0; i < arreglo.length; i++)
+	int tamano = tablero.length-1;
+	System.out.print("    ");
+	for(int i = 0; i <= tamano; ++i)
 	    {
-		for(int j = 0; j < arreglo.length; j++)
-		    if (j == arreglo[i])
-			System.out.print("Q");
-		    else
-			System.out.print("*");
+		System.out.print(i+" ");
+		
+	    }
+	System.out.println("\n");
+	for(int i  = 0; i <= tamano; ++i)
+	    {
+		System.out.print(i+"   ");
+		for(int j = 0; j <= tamano; ++j)
+		    {
+			System.out.print((tablero[i] == j ? "Q": "#")+ " ");
+			
+		    }
 		System.out.println();
 	    }
-	System.out.println();
     }
-    
     
     //tomado de interactiva
     public static boolean valido(int[] tablero)
     {
 	for(int i = 0; i < tablero.length; i++)
-	    for(int j = i+1; j < tablero.length; j++)
-		if(tablero[i] == tablero[j] || Math.abs(tablero[i]-tablero[j]) == Math.abs(i-j))
-		    return false;
-	return true;
+	    {
+		for(int j = i+1; j <tablero.length; j++)
+		    {
+			if(tablero[i] == tablero[j] || Math.abs(tablero[i]-tablero[j])== Math.abs(j-i))
+			    {
+				return true;	
+			    }
+		    }
+	    }
+	return false;
     }
     
     
@@ -131,6 +124,6 @@ public class Taller2
 	    {
 		System.out.println(p.get(i));
 		}*/
-	System.out.println(reinas(1));
+	reinas(2);
       }
 }
