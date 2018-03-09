a21ed75b9f5577f5760aa46522726a4ca7cb91e2
damesaa201710054010 <damesaa@eafit.edu.co>
2018-03-08 20:33:44 -0500

    punto uno listo, me falta limpiar y simplificar el codigo

--- talleres/taller06/codigo_estudiante/Taller6.java
+++ talleres/taller06/codigo_estudiante/Taller6.java
@@ -5,11 +5,40 @@
  */
 public class Taller6 {
 
-	public static int[] cambioGreedy(int n, int[] denominaciones) {
-		// complete...	}
-
-	public static int recorrido(Digraph g) {
-	    boolean[] visit =  new boolea[g.size()];
-	}
+    public static int[] cambioGreedy(int n, int[] denominaciones) {
+	int[] dv = new int[denominaciones.length];
+	int k = 0;
+	while(k < denominaciones.length )
+	    {
+		if(n-denominaciones[k] > 0)
+		    {
+			dv[k] = dv[k] + 1 ;
+			n = n - denominaciones[k];
+		    }
+		if(n - denominaciones[k] < 0)
+		    {
+			k++;
+		    }else if(n - denominaciones[k] == 0)
+		    {
+			n = n - denominaciones[k];
+			dv[k] = dv[k] + 1;
+			return dv;
+		    }
+	    }
+	return dv;
+    }
+    public static void main(String[] args)
+    {
+	int [] deno = {500, 300, 200, 50};
+	int [] dv = cambioGreedy(900, deno);
+	for(int i = 0; i < dv.length; i++)
+	    {
+		System.out.println(dv[i]);
+	    }
+	
+    }
+    /* public static int recorrido(Digraph g) {
+	boolean[] visit =  new boolea[g.size()];
+    }*/
 
 }
